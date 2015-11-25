ae9418c19a87b4462d7bb81287ba7c5ddc099400
kirandasika30 <kirandasika30@gmail.com>
2015-11-25 21:15:16 +0000

    changed

--- search/views.py
+++ search/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from insert_books import *
 import json
-# Create your views here.
-def home(reqeust):
+
+def home(request):
     return HttpResponse("Welcome", content_type="application/json")
     
-
+    
 @csrf_exempt
-def test_view(reqeust):
+def test(request):
     if request.method == 'POST':
         name = request.POST.get('name', "")
         return HttpResponse(json.dumps({'response': name}), content_type="application/json")
     else:
-        return HttpResponse(json.dumps({'response': 'Please send the correct request'}), 
+        return HttpResponse(json.dumps({'response': 'Please send the correct request'}),
                             content_type="application/json")
+                            
