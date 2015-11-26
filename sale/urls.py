a71eeb8f89ed7eb1a8fda2addf80737a9be3982c
kirandasika30 <kirandasika30@gmail.com>
2015-11-26 03:59:00 +0000

    changed

--- sale/urls.py
+++ sale/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'new_sale/', views.new_sale),
+    url(r'^new_sale/', views.new_sale),
+    url(r'^title_case_string/', views.title_case_string),
 ]
