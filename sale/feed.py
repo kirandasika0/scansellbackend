1155b149554d444e33e732de2a6f5fd10196e55a
kirandasika30 <kirandasika30@gmail.com>
2016-01-02 19:30:47 +0000

    changed feed alg

--- sale/feed.py
+++ sale/feed.py
@@ -2,6 +2,7 @@ import redis
 from .models import Sale
 from users_b.models import User
 from django.core import serializers
+from search.models import Book
 #Creating new redis server
 r = redis.Redis(host='pub-redis-18592.us-east-1-2.4.ec2.garantiadata.com',
                 port=18592,
@@ -23,13 +24,12 @@ def place_sale(sale, locale):
             
 def generate_feed(user_id):
     user = User.objects.get(user_id=user_id)
-    user_redis_key = user.redis_key
     user_locale = user.locale.split(',')
     #reversing the list
     user_locale.reverse()
     user_locality = user_locale[1]
     #getting all the sales
-    sales = Sales.objects.all()
+    sales = Sale.objects.all()
     feed_products = []
     for sale in sales:
         #check if the locality match and add it to the feed_products list
@@ -45,15 +45,15 @@ def generate_feed(user_id):
         
     #we have to serialize the data and return it back
     serialized_data = []
-    for book in feed_products:
-        latitude, longitude = book.geo_point.split(',')
-        product_data = {'id': book.id, 'seller_id': book.seller_id,
-                        'seller_username': book.seller_username,
-                        'book': serializers.serialize("json", [obj])[1:-1],
-                        'price': book.price,
-                        'location': book.location,
+    for sale in feed_products:
+        latitude, longitude = sale.geo_point.split(',')
+        product_data = {'id': sale.id, 'seller_id': sale.seller_id,
+                        'seller_username': sale.seller_username,
+                        'book': serializers.serialize("json", [Book.objects.get(pk=sale.book_id)])[1:-1],
+                        'price': sale.price,
+                        'location': sale.location,
                         'latitude': latitude,
                         'longitude': longitude,
-                        'pub_date': book.created_at}
+                        'pub_date': sale.created_at}
         serialized_data.append(product_data)
     return serialized_data
