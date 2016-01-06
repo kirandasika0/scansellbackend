4ba6ccd6c76a29f38d669ef33b82c4dfe53014d9
kirandasika30 <kirandasika30@gmail.com>
2016-01-06 00:18:59 +0000

    view change

--- sale/views.py
+++ sale/views.py
@@ -170,7 +170,7 @@ def get_feed(request):
 def sale_notification(request):
     if request.method == 'POST':
         #get the notif data 
-        return HttpResponse(type(request.body))
+        return HttpResponse(type(request.POST))
     else:
         return HttpResponse(json.dumps({'response': 'please send the correct request'}),
                             content_type="application/json")
