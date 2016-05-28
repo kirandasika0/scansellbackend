d11c3c6116337c1111a3f36ced5102f58f6d019a
kirandasika30 <kirandasika30@gmail.com>
2016-05-28 16:18:42 +0000

    d

--- sale/distance_module/setup.py
+++ sale/distance_module/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import Extension, setup
 from Cython.Distutils import build_ext
 
-distance_module = Extension('distance_module', sources=['distance_main.c', 'haversine_km.c', 'haversine_mi.c'])
+distance_module = Extension('distance_module', sources=['../../distance_main.c', 'haversine_km.c', 'haversine_mi.c'])
 
 setup(
 	cmdclass={'build_ext': build_ext},
