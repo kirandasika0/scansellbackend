964b073ce2ba571d1f72546609706732d7902bcd
kirandasika30 <kirandasika30@gmail.com>
2016-06-17 07:33:31 +0000

    change in urls in users_b

--- users_b/urls.py
+++ users_b/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^create_user/', views.create_user),
-    url(r'^location_update/', views.update_location),
+    url(r'^update_location/', views.update_location),
 ]
