c302e2b2f6867acf5ce0de8b8c5c91e30f8f2822
kirandasika30 <kirandasika30@gmail.com>
2016-10-08 18:15:57 +0000

    test

--- sale/models.py
+++ sale/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from location import Location
+import distance_module.distance_module import distance_km
 # Create your models here.
 # main sale model where the user can set the price of the book they are gonna
 # sell
@@ -23,7 +24,16 @@ class Sale(models.Model):
         return Location(latitude, longitude)
         
     def compareTo(self, otherSale, refLocation):
-        pass
+        selfLocation = self.getLocation()
+        selfDistance = distance_km(refLocation.latitude,
+                                    refLocation.longitude,
+                                    selfLocation.latitude,
+                                    selfLocation.longitude)
+        otherDistance = distance_km(refLocation.latitude,
+                                    refLocation.longitude,
+                                    otherSale.getLocation().latitude,
+                                    otherSale.getLocation().longitude)
+        return selfDistance > otherDistance
     class Meta:
         ordering = ['-created_at']
 
