986ed5b5b13e096da453825b7add27a2c80047e0
kirandasika30 <kirandasika30@gmail.com>
2016-10-08 18:12:45 +0000

    test

--- sale/distance_module/sales_sort.py
+++ sale/distance_module/sales_sort.py
@@ -3,5 +3,4 @@ from sale.models import Sale, SaleImage
 from users_b.models import User
 
 def sales_sort(sales, user_location):
-    ''' This function returns a list of sales sorted by distance '''
     pass
