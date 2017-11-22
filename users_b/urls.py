bdcee54ea67acf3659bb95197ed56a7ac9072577
Sai Kiran Dasika <kirandasika30@gmail.com>
2017-11-22 14:58:28 -0600

    URLconf for UpdateLocationView

--- users_b/urls.py
+++ users_b/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^create_user/', views.SignupView.as_view()),
-    url(r'^update_location/', views.update_location),
+    url(r'^update_location/', views.UpdateLocationView.as_view()),
     url(r'login/', views.LoginView.as_view()),
     url(r'my_sales/', views.mySales),
     url(r'all_users/', views.allUsers),
