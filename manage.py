7846b2c08d03ad770deb7284c769c41a40484a6c
Sai Kiran Dasika <kirandasika30@gmail.com>
2017-11-21 21:29:29 -0600

    change in manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-
+import pymysql
+pymysql.install_as_MySQLdb()
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scansell.settings")
 
     from django.core.management import execute_from_command_line
-    import pymysql
-    pymysql.install_as_MySQLdb()
+
     execute_from_command_line(sys.argv)
